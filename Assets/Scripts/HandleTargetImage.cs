28ef135a371ff20bf18bc3a0ce488daa02e5e1c0
Anton Kanin <antony.kanin@gmail.com>
2018-10-10 00:15:54 +0300

    adding video scripts

---
+++ Assets/Scripts/HandleTargetImage.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class HandleTargetImage : DefaultTrackableEventHandler
+{
+    [Tooltip("Delay before video will be played (seconds)")]
+    [SerializeField]
+    private float m_VideoDelay = 1f;
+
+    private float m_TimeStartPlayingVideo = 0;
+
+    private bool m_checkTimer = false;
+
+    protected override void OnTrackingFound()
+    {
+        m_checkTimer = true;
+        m_TimeStartPlayingVideo = Time.time + m_VideoDelay;
+    }
+
+    void Update()
+    {
+        if (m_checkTimer && m_TimeStartPlayingVideo < Time.time)
+        {
+            EnableAll();
+            m_checkTimer = false;
+        }
+    }
+
+    protected override void OnTrackingLost()
+    {
+        DisableAll();
+    }
+
+    private void EnableAll()
+    {
+        Debug.Log("Enabling children");
+        foreach (Transform childTransform in transform)
+        {
+            childTransform.gameObject.SetActive(true);
+        }
+    }
+
+    private void DisableAll()
+    {
+        Debug.Log("Disabling children");
+        foreach (Transform childTransform in transform)
+        {
+            childTransform.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ShowVideo.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class ShowVideo : MonoBehaviour {
+
+    [SerializeField] private VideoPlayer m_VideoPlayer = null;
+    [SerializeField] private float m_FadeOutTime = 1f;
+
+    private float m_EnabledTime;
+
+    void Start ()
+	{
+	    m_VideoPlayer = GetComponent<VideoPlayer>();
+	}
+
+    private void OnEnable()
+    {
+        m_EnabledTime = Time.time;
+        m_VideoPlayer.targetCameraAlpha = 0;
+    }
+
+    void Update()
+    {
+        float currentAlpha = m_VideoPlayer.targetCameraAlpha;
+        float alphaDiff = Mathf.Abs(1.0f - currentAlpha);
+        const float m_fadeRate = 10;
+        Debug.Log(m_VideoPlayer.targetCameraAlpha);
+
+        if (alphaDiff > 0.00001f)
+        {
+            m_VideoPlayer.targetCameraAlpha = currentAlpha + Time.deltaTime / 3.0f;
+        }
+
+        //if ((m_EnabledTime + m_FadeOutTime < Time.time) && (m_VideoPlayer.isPlaying == false))
+        //{
+        //    m_VideoPlayer.Play();
+        //}
+    }
+
+    // Update is called once per frame
+}
