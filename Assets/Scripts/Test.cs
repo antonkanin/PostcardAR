b3012373dd61231273b65389d530c8d09f21ba95
Anton Kanin <antony.kanin@gmail.com>
2018-10-14 22:13:13 +0300

    adding alphatest scene

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class Test : MonoBehaviour
+{
+    public float m_alpha = 1f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    GetComponent<VideoPlayer>().targetCameraAlpha = m_alpha;
+	}
+}
